5a2153bd633e2ab145cd760ceda3aa028cf28214
nickolasrm <nickolasrochamachado@gmail.com>
2022-11-13 17:32:50 -0300

    added model evaluation node
    * added metrics dataset in catalog
    
    * added evaluation parameters
    
    * added history metrics output
    
    Signed-off-by: nickolasrm <nickolasrochamachado@gmail.com>

--- src/hotelbookingcancellation/pipelines/data_science/pipeline.py
+++ src/hotelbookingcancellation/pipelines/data_science/pipeline.py
@@ -2,7 +2,7 @@
 
 from kedro.pipeline import Pipeline, node, pipeline
 
-from .nodes import optimize, split_train_test
+from .nodes import evaluate, optimize, split_train_test
 
 
 def create_pipeline() -> Pipeline:
@@ -20,6 +20,12 @@ def create_pipeline() -> Pipeline:
                 inputs=["x_train", "y_train", "params:optimize"],
                 outputs="model",
                 name="optimize",
+            ),
+            node(
+                func=evaluate,
+                inputs=["model", "x_test", "y_test", "params:evaluate"],
+                outputs="metrics",
+                name="evaluate",
             ),
         ]
     )
