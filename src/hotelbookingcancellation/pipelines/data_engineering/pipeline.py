4b768d5c14251e7731f7a9200448e454f8476ad8
nickolasrm <nickolasrochamachado@gmail.com>
2022-11-13 12:55:31 -0300

    added preprocessing pipeline
    * add exceptions for pylint variable names
    
    * added hotel_bookings and preprocessed bookings in catalog
    
    * added data_engineering pipeline
    
    * updated requirements with libs for reading from web and saving parquet
    
    * added data_engineering parameters for configuring preprocessing
    
    * updated gitignore
    
    Signed-off-by: nickolasrm <nickolasrochamachado@gmail.com>

--- src/hotelbookingcancellation/pipelines/data_engineering/pipeline.py
+++ src/hotelbookingcancellation/pipelines/data_engineering/pipeline.py
@@ -5,17 +5,17 @@ generated using Kedro 0.18.2
 
 from kedro.pipeline import Pipeline, node, pipeline
 
-from .nodes import node1
+from .nodes import preprocess_bookings
 
 
 def create_pipeline() -> Pipeline:
-    """Creates the pipeline for data engineering."""
+    """Creates the pipeline for preprocessing the raw data."""
     return pipeline(
         [
             node(
-                func=node1,
-                inputs=None,
-                outputs="io1",
+                func=preprocess_bookings,
+                inputs=["hotel_bookings", "params:preprocessing"],
+                outputs="preprocessed_hotel_bookings",
             )
         ]
     )
