fda3a51dadb165516a478a62933ea1eab517ffc8
nickolasrm <nickolasrochamachado@gmail.com>
2022-11-12 22:26:04 -0300

    added kedro project build for docker
    * added dummy serving pipeline
    
    * added dummy de and ds pipelines
    
    * added kedro build dockerfile
    
    * updated compose with api and training services
    
    Signed-off-by: nickolasrm <nickolasrochamachado@gmail.com>

---
+++ src/hotelbookingcancellation/pipelines/data_science/nodes.py
@@ -0,0 +1,9 @@
+"""
+This is a boilerplate pipeline 'data_science'
+generated using Kedro 0.18.2
+"""
+
+
+def node2(_: int):
+    """Dummy."""
+    return 2
---
+++ src/hotelbookingcancellation/pipelines/scoring/nodes.py
@@ -0,0 +1,9 @@
+"""
+This is a boilerplate pipeline 'scoring'
+generated using Kedro 0.18.2
+"""
+
+
+def node3():
+    """Dummy."""
+    return 3
