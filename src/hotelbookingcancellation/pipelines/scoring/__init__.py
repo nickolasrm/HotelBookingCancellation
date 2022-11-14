b6d1e7cd0029e240794cd9cff098f0d3536d9d11
nickolasrm <nickolasrochamachado@gmail.com>
2022-11-13 22:21:31 -0300

    added model loader dataset for apis
    * added a dataset that loads a model from registry
    
    * added auto update in interval
    
    * added treatments for missing model
    
    Signed-off-by: nickolasrm <nickolasrochamachado@gmail.com>

--- src/hotelbookingcancellation/pipelines/scoring/__init__.py
+++ src/hotelbookingcancellation/pipelines/scoring/__init__.py
@@ -3,6 +3,7 @@ This is a boilerplate pipeline 'scoring'
 generated using Kedro 0.18.2
 """
 
+from .mlflow_model_loader_dataset import MlflowModelLoaderDataSet
 from .pipeline import create_pipeline
 
 __all__ = ["create_pipeline"]
